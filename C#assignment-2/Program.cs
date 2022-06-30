e1cd1cd92bafbe443036eb7657d941455984aa8a
Emanesk19 <101937600+Emanesk19@users.noreply.github.com>
2022-06-30 15:24:43 +0300

    loaded

---
+++ C#assignment-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] nums = new int[10];
+for(int i = 0; i < nums.Length - 1; i++)
+{
+    Console.WriteLine("Enter number : " + (i + 1));
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for(int i = 0; i < nums.Length - 1; i++)
+{
+    if (nums[i] == nums[i + 1])
+    {
+        Console.WriteLine("The duplicate value is : " + nums[i]);
+    }
+}
